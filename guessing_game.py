f939c792cdd5797c4f99c39b71f2cdf57dad68de
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-05-31 10:25:56 -0400

    Cleaned up unneeded code

--- guessing_game.py
+++ guessing_game.py
@@ -39,12 +39,3 @@ while game.solved == False:
 
 print(f"{last_guess} was correct! Good job!")
 
-# class Dog:
-#     species = "Canis familiaris"
-
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-
-# nerd = Dog("Nerd", 43)
-# miles = Dog("Miles", 4)

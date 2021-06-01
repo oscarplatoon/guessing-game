2681c16c28753f44fe3a035ab5918585ce9a82bf
donkelly55331 <donkelly55331@gmail.com>
2021-06-01 18:27:48 -0500

    Don and Musa Guessing Game

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,57 @@
 class GuessingGame():
+
+
+    # init method or constructor 
+    def __init__(self, num):
+        self.number_to_guess = num
+        self.slvd = False
+   
+    def guess(self, user_guess):
+        if user_guess < self.number_to_guess:
+            return 'low'
+        elif user_guess > self.number_to_guess:
+            return 'high'
+        else:
+            self.slvd = True
+            return 'right'
+
+    def solved(self):
+        return self.slvd
+
+import random
+
+# ----- DRIVER CODE -----14
+
+game = GuessingGame(random.randint(1,100))
+last_guess  = None
+last_result = None
+
+while game.solved() == False:
+    
+    guess = 0
+    while guess == 0:
+        try:
+            guess = int(input("Enter your guess: "))
+        except: 
+            print("Please enter a number from 1 to 100.")
+
+    result = game.guess(guess)
+    if result != 'right':
+        print(f"Oops! Your last guess ({guess}) was {result}.")
+        print("")
+    
+print(f"{guess} was correct!")
+
+
+
+
+# game = GuessingGame(10)
+
+# game.solved()   # => False
+
+# game.guess(5)  # => 'low'
+# game.guess(20) # => 'high'
+# game.solved()   # => False
+
+# game.guess(10) # => 'correct'
+# game.solved()   # => True

5626ea0f3ed87314f57022f5d9368c928d445413
awhitford10 <awhitford@Andrews-Mac-mini.local>
2021-06-01 16:56:51 -0400

    Andrew and John_guessing game_solution

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,39 @@
+import random
+
 class GuessingGame():
+    last_guess = None
+
+    def __init__(self,answer):
+        self.answer = answer
+
+    def guess(self,user_guess):
+        self.last_guess = user_guess
+        if(int(user_guess) == self.answer):
+            return 'correct'
+        elif(int(user_guess) < self.answer):
+            return 'low'
+        if(int(user_guess) > self.answer):
+            return 'high'
+
+    def solved(self):
+        if(self.last_guess == str(self.answer)):
+            return True
+        else:
+            return False
+
+
+
+game = GuessingGame(random.randint(1,100))
+last_guess  = None
+last_result = None
+
+while game.solved() == False:
+  if last_guess != None: 
+    print(f"Oops! Your last guess ({last_guess}) was {last_result}.")
+    print("")
+
+  last_guess  = input("Enter your guess: ")
+  last_result = game.guess(last_guess)
+
+
+print(f"{last_guess} was correct!")

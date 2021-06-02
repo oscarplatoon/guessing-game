4bdb625328d58e9279481c367365626851bc8303
vzbrunstad <vzbrunstad@msn.com>
2021-06-01 21:19:27 -0500

    Vince B Guessing Game Solution

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,22 +1,25 @@
 
+
 class GuessingGame():
-    user_guess = False
+    last_guess = None
 
     def __init__ (self, answer):
         self.answer = answer
-    
+
     def guess(self, user_guess):
-        print(self.answer)
-        return False
+        self.last_guess = user_guess
+
+        if int(user_guess) == self.answer:
+            return 'Correct'
+        if int(user_guess) < self.answer:
+            return 'Low'
+        if int(user_guess) > self.answer:
+            return "High"
 
 
-# game = GuessingGame(10)
+    def solved(self):
+        if self.last_guess == str(self.answer):
+            return True
+        else:
+            return False
 
-# game.solved()   # => False
-
-# game.guess(5)  # => 'low'
-# game.guess(20) # => 'high'
-# game.solved()   # => False
-
-# game.guess(10) # => 'correct'
-# game.solved()   # => True
--- guessing_game.py
+++ guessing_game.py
@@ -1,27 +1,5 @@
 import random
-
-class GuessingGame():
-    last_guess = None
-
-    def __init__ (self, answer):
-        self.answer = answer
-
-    def guess(self, user_guess):
-        self.last_guess = user_guess
-
-        if int(user_guess) == self.answer:
-            return 'Correct'
-        if int(user_guess) < self.answer:
-            return 'Low'
-        if int(user_guess) > self.answer:
-            return "High"
-
-
-    def solved(self):
-        if self.last_guess == str(self.answer):
-            return True
-        else:
-            return False
+from GuessingGame import GuessingGame
 
 game = GuessingGame(random.randint(1,100))
 last_guess  = None
@@ -36,7 +14,5 @@ while game.solved() == False:
     
     last_guess  = input("Enter your guess: ")
     last_result = game.guess(last_guess)
-
-
 
 print(f"{last_guess} was correct!")
